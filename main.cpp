ae6a0d27ddf7474a7359c9427449033fae28dbbc
Marcel Jar <marcel.jar@senecacollege.ca>
2017-02-02 21:31:52 -0500

    added an exclamation mark

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(){
 
     char message[128];
-    std::cout <<"Hello world" << std::endl;
+    std::cout <<"Hello world!" << std::endl;
     std::cin.getline(message,sizeof(message));
 
     return 0;
